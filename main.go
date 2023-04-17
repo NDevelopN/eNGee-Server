f5967980f821fa8cf71f167ae63ee34cb5c8a27e
Nathan <NdeveloperN@gmail.com>
2023-04-17 13:26:18 +0100

    Simplify main and server files

--- main.go
+++ main.go
@@ -1,68 +1,19 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
-	"net/http"
 	"time"
-
-	"github.com/gorilla/websocket"
 
 	server "Engee-Server/server"
 )
 
-var mux = map[string]func(http.ResponseWriter, *http.Request){
-	"/":  def,
-	"/s": socket,
-}
-
-var upgrader = websocket.Upgrader{}
-
 func main() {
 	log.Println("Welcome to Engee-Server!")
 	go func() {
-		server.Serve(mux)
+		server.Serve()
 	}()
 
 	for {
 		time.Sleep(1 * time.Second)
 	}
-}
-
-// default function, return error, but log request
-func def(writer http.ResponseWriter, request *http.Request) {
-	reqBody, _ := ioutil.ReadAll(request.Body)
-	log.Print("Request to \"/\": \n")
-	log.Print(reqBody)
-	log.Print("\n")
-
-	http.Error(writer, "Invalid request", http.StatusBadRequest)
-}
-
-func socket(writer http.ResponseWriter, request *http.Request) {
-	conn, err := upgrader.Upgrade(writer, request, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println("Websocket connection established")
-	defer conn.Close()
-
-	for {
-		mt, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			break
-		}
-
-		input := string(message)
-
-		err = conn.WriteMessage(mt, []byte(input))
-		if err != nil {
-			log.Println("write failed:", err)
-			break
-		}
-
-	}
-
 }
--- server/server.go
+++ server/server.go
@@ -1,32 +1,25 @@
 package server
 
 import (
-	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
-
-var mux map[string]func(http.ResponseWriter, *http.Request)
-
-type Message struct {
-	Text string
-}
 
 type myHandler struct{}
 
 /**TODO: Review and see which are needed */
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
+	(*w).Header().Set("Access-Control-Allow-Methods", "*")
+	(*w).Header().Set("Access-Control-Allow-Headers", "*")
 }
 
 func (h *myHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	// Handle CORS issues TODO: Review CORS in detail
 	enableCors(&writer)
 	if request.Method == "OPTIONS" {
+		log.Println(request.URL)
 		writer.WriteHeader(http.StatusOK)
 		writer.Write([]byte(""))
 		return
@@ -40,28 +33,12 @@ func (h *myHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request)
 	http.Error(writer, "Invalid route"+request.URL.Path, http.StatusNotFound)
 }
 
-func Write(writer http.ResponseWriter, request *http.Request) {
-
-	//Decode body into formated form
-	var msg Message
-	err := json.NewDecoder(request.Body).Decode(&msg)
-
-	if err != nil {
-		http.Error(writer, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	fmt.Println(msg)
-}
-
-func Serve(inMux map[string]func(http.ResponseWriter, *http.Request)) error {
+func Serve() error {
 	server := http.Server{
 		Addr:        ":8080",
 		Handler:     &myHandler{},
 		ReadTimeout: 5 * time.Second,
 	}
-
-	mux = inMux
 
 	return server.ListenAndServe()
 }
