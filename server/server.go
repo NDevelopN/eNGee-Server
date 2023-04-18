128bd9b4fa2f2e76a82c07546530493a9c03191f
Nathan <NdeveloperN@gmail.com>
2023-04-18 13:27:56 +0100

    Add nested Muxes

--- server/server.go
+++ server/server.go
@@ -3,7 +3,10 @@ package server
 import (
 	"log"
 	"net/http"
+	"strings"
 	"time"
+
+	g "Engee-Server/gameRoom"
 )
 
 type myHandler struct{}
@@ -13,6 +16,12 @@ func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "*")
 	(*w).Header().Set("Access-Control-Allow-Headers", "*")
+}
+
+var mux = map[string]func(http.ResponseWriter, *http.Request){
+	"":       ReMux,
+	"server": ReMux,
+	"game":   g.ReMux,
 }
 
 func (h *myHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
@@ -25,12 +34,16 @@ func (h *myHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request)
 		return
 	}
 
+	// Get the base path and pass to relevant ReMux
+	path := strings.Split(request.URL.Path, "/")[1]
+	log.Printf("Path: %s", path)
+
 	//Implement route forwarding, ensure there is a route established for the request
-	if handler, ok := mux[request.URL.Path]; ok {
+	if handler, ok := mux[path]; ok {
 		handler(writer, request)
 		return
 	}
-	http.Error(writer, "Invalid route"+request.URL.Path, http.StatusNotFound)
+	http.Error(writer, "Invalid route: "+request.URL.Path, http.StatusNotFound)
 }
 
 func Serve() error {
