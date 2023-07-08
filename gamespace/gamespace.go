ea3837403a105650162f5aa7cd15377d6e3a8cb1
Nathan <NdeveloperN@gmail.com>
2023-07-08 11:43:42 +0100

    Implement GS End Route

--- gamespace/gamespace.go
+++ gamespace/gamespace.go
@@ -63,6 +63,15 @@ func reset(msg utils.GameMsg, game utils.Game, handler HandlerFunc) (utils.GameM
 	return handler(msg, game)
 }
 
+func end(msg utils.GameMsg, game utils.Game, handler HandlerFunc) (utils.GameMsg, error) {
+	err := End(msg.GID, msg.UID)
+	if err != nil {
+		return replyError(msg, err)
+	}
+
+	return handler(msg, game)
+}
+
 func GamespaceHandle(msg utils.GameMsg) (utils.GameMsg, error) {
 	game, err := g.GetGame(msg.GID)
 	if err != nil {
@@ -104,7 +113,12 @@ func GamespaceHandle(msg utils.GameMsg) (utils.GameMsg, error) {
 		} else {
 			return replyError(msg, errNotLeader)
 		}
-
+	case "End":
+		if leader {
+			return end(msg, game, handler)
+		} else {
+			return replyError(msg, errNotLeader)
+		}
 	default:
 		return replyError(msg, fmt.Errorf("unknown message type: %v", msg.Type))
 	}
