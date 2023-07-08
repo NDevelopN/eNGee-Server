c9b9ccd62b29baa65ef78824b0d58d2a604e5e47
Nathan <NdeveloperN@gmail.com>
2023-07-08 10:21:09 +0100

    Add GS/Leader Tests

---
+++ gamespace/leader.go
@@ -0,0 +1,27 @@
+package gamespace
+
+import utils "Engee-Server/utils"
+
+func Pause(gid string, lid string) error {
+	return nil
+}
+
+func Start(gid string, lid string) error {
+	return nil
+}
+
+func Reset(gid string, lid string) error {
+	return nil
+}
+
+func End(gid string, lid string) error {
+	return nil
+}
+
+func Rules(gid string, lid string, game utils.Game) error {
+	return nil
+}
+
+func Remove(gid string, lid string, tid string) error {
+	return nil
+}
