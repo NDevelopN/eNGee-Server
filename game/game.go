aa1a1023c6e13cf22c940a3245eeffa7277615f7
Nathan <NdeveloperN@gmail.com>
2023-07-08 09:18:26 +0100

    Implement CreateGame

--- game/game.go
+++ game/game.go
@@ -1,8 +1,33 @@
 package game
 
-import u "Engee-Server/utils"
+import (
+	db "Engee-Server/database"
+	u "Engee-Server/utils"
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 func CreateGame(g u.Game) (string, error) {
+	if g.Name == "" {
+		return "", fmt.Errorf("provided game name is empty")
+	}
+
+	if g.Type == "" {
+		return "", fmt.Errorf("provided game type is empty")
+	}
+
+	if g.MinPlrs > g.MaxPlrs {
+		return "", fmt.Errorf("provided minPlrs is greater than provided maxPlrs")
+	}
+
+	g.GID = uuid.NewString()
+	g.Status = "Lobby"
+
+	err := db.CreateGame(g)
+	if err != nil {
+		return "", fmt.Errorf("failed to create game in database: %v", err)
+	}
 
 	return g.GID, nil
 }
