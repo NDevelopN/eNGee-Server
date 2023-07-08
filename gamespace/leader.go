3f7df255c5def13a61b124823c5c935ca14b14a0
Nathan <NdeveloperN@gmail.com>
2023-07-08 10:36:26 +0100

    Implement Pause

--- gamespace/leader.go
+++ gamespace/leader.go
@@ -1,8 +1,55 @@
 package gamespace
 
-import utils "Engee-Server/utils"
+import (
+	g "Engee-Server/game"
+	utils "Engee-Server/utils"
+	"fmt"
+)
+
+func checkLeader(gid string, lid string) (utils.Game, error) {
+	game, err := g.GetGame(gid)
+
+	if err != nil {
+		return game, fmt.Errorf("could not find game")
+	}
+
+	if game.Leader != lid {
+		return game, fmt.Errorf("game leader and provided ID do not match")
+	}
+
+	return game, nil
+}
+
+func sendUpdate(gid string) error {
+	//TODO
+
+	return nil
+}
 
 func Pause(gid string, lid string) error {
+	game, err := checkLeader(gid, lid)
+	if err != nil {
+		return err
+	}
+
+	if game.Status == "Pause" {
+		game.Status = game.OldStatus
+		game.OldStatus = ""
+	} else {
+		game.OldStatus = game.Status
+		game.Status = "Pause"
+	}
+
+	err = g.UpdateGame(game)
+	if err != nil {
+		return fmt.Errorf("failed to update game: %v", err)
+	}
+
+	err = sendUpdate(gid)
+	if err != nil {
+		return fmt.Errorf("failed to broadcast update: %v", err)
+	}
+
 	return nil
 }
 
