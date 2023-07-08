45df7fc21915d529b41740c1aa52691a841c0069
Nathan <NdeveloperN@gmail.com>
2023-07-08 13:13:29 +0100

    Create test outline for consequences handler

---
+++ consequences/handler_test.go
@@ -0,0 +1,39 @@
+package consequences
+
+import "testing"
+
+func TestInit(t *testing.T) {
+
+}
+
+func TestPause(t *testing.T) {
+
+}
+
+func TestUnpause(t *testing.T) {
+
+}
+
+func TestEnd(t *testing.T) {
+
+}
+
+func TestReset(t *testing.T) {
+
+}
+
+func TestRules(t *testing.T) {
+
+}
+
+func TestRemove(t *testing.T) {
+
+}
+
+func TestStatus(t *testing.T) {
+
+}
+
+func TestLeave(t *testing.T) {
+
+}
