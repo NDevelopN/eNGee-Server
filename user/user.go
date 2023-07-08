3d8660bfc43d2c01c278bdd6fd02d461f185b108
Nathan <NdeveloperN@gmail.com>
2023-07-08 09:36:19 +0100

    Implement CreateUser

--- user/user.go
+++ user/user.go
@@ -1,9 +1,31 @@
 package user
 
-import utils "Engee-Server/utils"
+import (
+	db "Engee-Server/database"
+	utils "Engee-Server/utils"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 func CreateUser(u utils.User) (string, error) {
-	return "", nil
+	if u.Name == "" {
+		return "", errors.New("user name is empty")
+	}
+
+	if u.GID != "" {
+		return "", fmt.Errorf("a new user should not have a GID: %v", u.GID)
+	}
+
+	u.UID = uuid.NewString()
+
+	err := db.CreateUser(u)
+	if err != nil {
+		return "", fmt.Errorf("could not create user in database: %v", err)
+	}
+
+	return u.UID, nil
 }
 
 func GetUser(uid string) (utils.User, error) {
