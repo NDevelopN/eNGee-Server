3930022ec27fb604d09fe28320772a3bafd66d93
Nathan <NdeveloperN@gmail.com>
2023-07-08 10:51:17 +0100

    Implement Start

--- gamespace/leader.go
+++ gamespace/leader.go
@@ -2,8 +2,10 @@ package gamespace
 
 import (
 	g "Engee-Server/game"
+	u "Engee-Server/user"
 	utils "Engee-Server/utils"
 	"fmt"
+	"log"
 )
 
 func checkLeader(gid string, lid string) (utils.Game, error) {
@@ -22,6 +24,19 @@ func checkLeader(gid string, lid string) (utils.Game, error) {
 
 func sendUpdate(gid string) error {
 	//TODO
+
+	return nil
+}
+
+func allPlayerStatusUpdate(plrs []utils.User, status string) error {
+	for _, plr := range plrs {
+		plr.Status = status
+
+		err := u.UpdateUser(plr)
+		if err != nil {
+			return fmt.Errorf("failed to update user: %v", err)
+		}
+	}
 
 	return nil
 }
@@ -54,6 +69,56 @@ func Pause(gid string, lid string) error {
 }
 
 func Start(gid string, lid string) error {
+	game, err := checkLeader(gid, lid)
+	if err != nil {
+		return err
+	}
+
+	if game.Status != "Lobby" && game.OldStatus != "Lobby" {
+		return fmt.Errorf("cannot start game that is not in lobby state")
+	}
+
+	if game.CurPlrs < game.MinPlrs {
+		return fmt.Errorf("game does not have enough players to start game")
+	}
+
+	plrs, err := g.GetGamePlayers(gid)
+	if err != nil {
+		return fmt.Errorf("could not get game players: %v", err)
+	}
+
+	//TODO is there a better way?
+	ready := 0
+	for i, plr := range plrs {
+		log.Printf("player %d: %v", i, plr)
+		if plr.Status == "Ready" {
+			ready++
+		}
+	}
+
+	//TODO add toggle for this requirement
+	if ready <= game.CurPlrs/2 {
+		return fmt.Errorf("less than half of the game players are ready (%d/%d)", ready, game.CurPlrs)
+	}
+
+	game.Status = "Play"
+	game.OldStatus = ""
+
+	err = allPlayerStatusUpdate(plrs, "Play")
+	if err != nil {
+		return err
+	}
+
+	err = g.UpdateGame(game)
+	if err != nil {
+		return fmt.Errorf("failed to update game: %v", err)
+	}
+
+	err = sendUpdate(gid)
+	if err != nil {
+		return fmt.Errorf("failed to broadcast update: %v", err)
+	}
+
 	return nil
 }
 
