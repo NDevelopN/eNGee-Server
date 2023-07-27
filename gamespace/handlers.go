7d8dbaa52a62c537c4bccc89689ca6b29db3cb2b
Nathan <NdeveloperN@gmail.com>
2023-07-27 09:26:53 +0100

    Test handler reply error when not default msgtype

--- gamespace/handlers.go
+++ gamespace/handlers.go
@@ -3,12 +3,36 @@ package gamespace
 import (
 	consequences "Engee-Server/consequences"
 	utils "Engee-Server/utils"
+	"errors"
 )
 
 type HandlerFunc func(msg utils.GameMsg) (utils.GameMsg, error)
 
 func TestHandler(msg utils.GameMsg) (utils.GameMsg, error) {
-	return utils.ReplyACK(msg), nil
+	switch msg.Type {
+	case "Init":
+		break
+	case "Start":
+		break
+	case "Reset":
+		break
+	case "End":
+		break
+	case "Pause":
+		break
+	case "Remove":
+		break
+	case "Rules":
+		break
+	case "Status":
+		break
+	case "Leave":
+		break
+	default:
+		return utils.ReplyError(msg, errors.New("invalid message Type"))
+	}
+
+	return utils.ReplyACK(msg, "TestMessage Accepted")
 }
 
 var TypeHandlers = map[string]HandlerFunc{
