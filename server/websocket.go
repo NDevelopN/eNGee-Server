db10a11176fd0aac57a8cf8ee1a02a02cd7d7595
Nathan <NdeveloperN@gmail.com>
2023-08-07 09:01:21 +0100

    Include locks on connpool in ws incoming handler

--- server/websocket.go
+++ server/websocket.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
@@ -107,60 +106,66 @@ func handleClose(code int, text string) error {
 	return fmt.Errorf("connection closed: %v", text)
 }
 
+func incoming(conn *utils.Conn, gid string, uid string) error {
+
+	messageType, data, err := conn.V.ReadMessage()
+	if err != nil {
+		log.Printf("[Close] connection closed: %v", err)
+		utils.RemoveConnection(gid, uid)
+
+		return err
+	}
+
+	if messageType != websocket.TextMessage {
+		log.Printf("[Error] Received unexpected message type: %v", messageType)
+		return nil
+	}
+
+	var msg = utils.GameMsg{}
+
+	err = json.Unmarshal(data, &msg)
+	if err != nil {
+		log.Printf("[Error] Cannot unmarshal received message: %v", err)
+		return nil
+	}
+
+	msg, err = gs.GamespaceHandle(msg)
+	if err != nil {
+		log.Printf("[Error] Handling message: %v", err)
+	}
+
+	if msg.UID == "" {
+		if err != nil {
+			log.Printf("[Error] Error in GS handle, but no msg to return")
+		}
+		return nil
+	}
+
+	reply, err := json.Marshal(msg)
+	if err != nil {
+		log.Printf("[Error] Failed to marshal message: %v", err)
+		return nil
+	}
+
+	conn.Mu.Lock()
+	defer conn.Mu.Unlock()
+
+	conn.V.WriteMessage(websocket.TextMessage, reply)
+	return nil
+}
+
 func handleIncoming(gid string, uid string) {
 	pool, err := utils.GetConnections(gid)
 	if err != nil {
 		log.Printf("[Error] getting connection for handler: %v", err)
 	}
+
 	conn := pool[uid]
 
 	for utils.CheckConnection(gid, uid) {
-		messageType, data, err := conn.ReadMessage()
+		err = incoming(conn, gid, uid)
 		if err != nil {
-			if strings.Split(err.Error(), ":")[0] == "connection closed" {
-				log.Printf("[Close] connection closed: %v", err)
-				utils.RemoveConnection(gid, uid)
-				msg := utils.GameMsg{
-					Type: "Leave",
-					GID:  gid,
-					UID:  uid,
-				}
-
-				_, err = gs.GamespaceHandle(msg)
-				if err != nil {
-					conn.Close()
-				}
-				return
-			} else {
-				log.Printf("[Error] Failed to read message: %v", err)
-				continue
-			}
+			return
 		}
-
-		if messageType != websocket.TextMessage {
-			log.Printf("[Error] Received unexpected message type: %v", messageType)
-			continue
-		}
-
-		var msg = utils.GameMsg{}
-
-		err = json.Unmarshal(data, &msg)
-		if err != nil {
-			log.Printf("[Error] Cannot unmarshal received message: %v", err)
-			continue
-		}
-
-		msg, err = gs.GamespaceHandle(msg)
-		if err != nil {
-			log.Printf("[Error] Handling message: %v", err)
-		}
-
-		reply, err := json.Marshal(msg)
-		if err != nil {
-			log.Printf("[Error] Failed to marshal message: %v", err)
-			continue
-		}
-
-		conn.WriteMessage(websocket.TextMessage, reply)
 	}
 }
