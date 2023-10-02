fc392f6fbf1c473564e4522395c364a5045b2332
Nathan Murphy <NDeveloperN@gmail.com>
2023-10-02 09:52:02 +0100

    Enable loading config from ENV

--- main.go
+++ main.go
@@ -12,20 +12,37 @@ import (
 
 var config utils.Config
 
+func loadEnv() {
+	port, found := os.LookupEnv("SPORT")
+	if found {
+		config.Server.Port = port
+
+		config.Database.Host = os.Getenv("DHOST")
+		config.Database.Host = os.Getenv("DPORT")
+		config.Database.Host = os.Getenv("DUSER")
+		config.Database.Host = os.Getenv("DPASS")
+		config.Database.Host = os.Getenv("DNAME")
+	} else {
+		log.Printf("Environment doesn't seem to be set. Checking config file.")
+		loadConfig()
+	}
+}
+
 func loadConfig() {
 	data, err := os.ReadFile("config.json")
 	if err != nil {
-		log.Fatalf("Could not read config file: %v", err)
+		log.Printf("Could not read config file: %v", err)
 	}
 
 	err = json.Unmarshal(data, &config)
 	if err != nil {
 		log.Fatalf("Could not read config data: %v", err)
 	}
+
 }
 
 func main() {
-	loadConfig()
+	loadEnv()
 	database.InitDB(config)
 	handlers.Init()
 	server.Serve(config)
