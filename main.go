0d6eb10c9ed36d4da736aa8cf893b941254c956a
Nathan Murphy <NDeveloperN@gmail.com>
2023-10-02 11:27:45 +0100

    Fix Env loading

--- main.go
+++ main.go
@@ -13,15 +13,15 @@ import (
 var config utils.Config
 
 func loadEnv() {
-	port, found := os.LookupEnv("SPORT")
+	port, found := os.LookupEnv("SERVER_INNER")
 	if found {
 		config.Server.Port = port
 
-		config.Database.Host = os.Getenv("DHOST")
-		config.Database.Host = os.Getenv("DPORT")
-		config.Database.Host = os.Getenv("DUSER")
-		config.Database.Host = os.Getenv("DPASS")
-		config.Database.Host = os.Getenv("DNAME")
+		config.Database.Host = os.Getenv("POSTGRES_HOST")
+		config.Database.Port = os.Getenv("POSTGRES_OUTER")
+		config.Database.User = os.Getenv("POSTGRES_USER")
+		config.Database.Pass = os.Getenv("POSTGRES_PASS")
+		config.Database.Name = os.Getenv("POSTGRES_NAME")
 	} else {
 		log.Printf("Environment doesn't seem to be set. Checking config file.")
 		loadConfig()
