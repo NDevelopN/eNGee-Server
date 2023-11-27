0e96073047c12cb295242e7e65a36264c72bccbd
NDevelopN <NDevelopN@gmail.com>
2023-11-27 15:31:01 +0000

    Test multiple Room Creations

--- room/room_test.go
+++ room/room_test.go
@@ -3,11 +3,28 @@ package room
 import "testing"
 
 const testRoomName = "Test Room"
+const newRoomName = "New Room"
 
 func TestCreateRoom(t *testing.T) {
 	id, err := CreateRoom(testRoomName)
 	if id == "" || err != nil {
 		t.Fatalf(`CreateRoom(Valid) = %q, %v, want "uuid", nil`, id, err)
+	}
+}
+
+func TestCreateUniqueNameRooms(t *testing.T) {
+	CreateRoom(testRoomName)
+	id, err := CreateRoom(newRoomName)
+	if id == "" || err != nil {
+		t.Fatalf(`CreateRoom(Unique Name) = %q, %v, want "uuid", nil`, id, err)
+	}
+}
+
+func TestCreateSameNameRooms(t *testing.T) {
+	CreateRoom(testRoomName)
+	id, err := CreateRoom(testRoomName)
+	if id == "" || err != nil {
+		t.Fatalf(`CreateRoom(Same Name) = %q, %v, want "uuid", nil`, id, err)
 	}
 }
 
