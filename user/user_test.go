9d4f36b5924e0667726e22e72f3717868ef18a9c
NDevelopN <NDevelopN@gmail.com>
2023-11-27 14:49:04 +0000

    Test Creation of multiple users

--- user/user_test.go
+++ user/user_test.go
@@ -26,6 +26,22 @@ func TestCreateUser(t *testing.T) {
 	}
 }
 
+func TestCreateUniqueNameUsers(t *testing.T) {
+	CreateUser(testUserName)
+	id, err := CreateUser(newTestName)
+	if id == "" || err != nil {
+		t.Fatalf(`CreateUser(Unique Name) = %q, %v, want "uuid", nil`, id, err)
+	}
+}
+
+func TestCreateSameNameUsers(t *testing.T) {
+	CreateUser(testUserName)
+	id, err := CreateUser(testUserName)
+	if id == "" || err != nil {
+		t.Fatalf(`CreateUser(Same Name) = %q, %v, want "uuid", nil`, id, err)
+	}
+}
+
 func TestCreateUserNoName(t *testing.T) {
 	id, err := CreateUser("")
 	if id != "" || err == nil {
