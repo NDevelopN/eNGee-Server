2a4468e564d8d74979fd78f935b9940b1ef7d4e3
NDevelopN <NDevelopN@gmail.com>
2023-11-27 12:30:40 +0000

    Remove all old files for clean workspace

--- main.go
+++ main.go
@@ -1,15 +1,5 @@
 package main
 
-import (
-	"Engee-Server/database"
-	"Engee-Server/handlers"
-	"Engee-Server/server"
-	"Engee-Server/utils"
-)
-
 func main() {
-	config := utils.LoadEnv()
-	database.InitDB(config)
-	handlers.Init()
-	server.Serve(config)
+
 }
